da1fd0cdf260164bd98df8df76259f67cd9057f7
Ms. Gallant <patricia.gallant@neric.org>
2024-01-29 13:12:06 -0500

    Add test project

--- LogicLessons/BooleanVariables/ComparisonOperators.cs
+++ LogicLessons/BooleanVariables/ComparisonOperators.cs
@@ -1,20 +1,37 @@
 ﻿using System;
+
 namespace BooleanVariables
 {
     public static class ComparisonOperators
     {
         public static void Activity2()
         {
-            double timeToDinner = 4;
-            double distance = 95;
-            double rate = 30;
+            /// Step 1
+            // Create a double variable named timeToDinner and 
+            // save the difference in hours between 2:00pm and 6:00pm.
 
-            double tripDuration = distance / rate;
 
-            bool answer = tripDuration <= timeToDinner;
+            /// Step 2
+            // Save the value 95 to a double variable named distance.
 
-            Console.WriteLine(answer);
+
+            // Save the value 30 to a double variable named rate.
+
+
+            // Step 3
+            //Write out the expression and save it to the variable tripDuration.
+
+
+            // Step 4
+            // Create a bool variable named answer and assign its value
+            // using the the appropriate boolean expression comparison
+            // that checks if tripDuration is less than or equal to timeToDinner.
+
+
+            // Step 5
+            // Print answer to the console
+
+            }
         }
-    }
 }
 
